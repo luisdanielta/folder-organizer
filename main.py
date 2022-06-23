ecd1f35bd747d79f048895a9830fd6af9f2a30f5
luis daniel <torresluisdaniel3@gmail.com>
2022-06-23 00:53:47 -0500

    update

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
-import sys, os, base64, json, re, subprocess, time
-from sysconfig import get_path
+import sys
+import os
+import base64
+import json
+import re
+import subprocess
+import time
 from tkinter import filedialog, ttk
 from tkinter import Tk
 
+
 class Window(Tk):
 
-    BUTTONS = ["Video", "Audio", "Image", "Document", "Compressed", "Program", "Other", "All"]
+    BUTTONS = {"Video": [], "Audio": [], "Image": [], "Document": [],
+               "Compressed": [], "Program": [], "Other": [], "All": ["All"]}
+    ADOBE = {
+        "Photoshop": ["psd"],
+        "Illustrator": ["ai", "eps", "pdf"]
+    }
     OS = sys.platform
     PATH: str
 
@@ -16,9 +27,12 @@ class Window(Tk):
         self.resizable(False, False)
 
         folder = ttk.Button(self, text="Select Folder", command=self.getPath)
-        folder.grid(row=0, column=0, padx=5, pady=5, sticky="NSEW")
+        folder.grid(row=0, padx=5, pady=5, sticky="NSEW")
 
         self.content()
+
+        self.exit = ttk.Button(self, text="Exit", command=self.destroy)
+        self.exit.grid(row=4, padx=5, pady=5, sticky="NSEW")
 
     def getPath(self):
         if self.OS == "win32":
@@ -35,7 +49,7 @@ class Window(Tk):
         self.textPath.grid(row=1, padx=5, pady=5, sticky="NSEW")
 
     def content(self):
-        self.frame = ttk.Frame(self)
+        self.frame = ttk.LabelFrame(self, text="File Groups")
         self.frame.grid(row=2, padx=5, pady=5)
 
         # row and column numbers orignaly set to 0
@@ -48,7 +62,19 @@ class Window(Tk):
             if c == 4:
                 r += 1
                 c = 0
-        
+
+        self.adobe = ttk.LabelFrame(self, text="Adobe")
+        self.adobe.grid(row=3, padx=5, pady=5, sticky="NSEW")
+        r = 0
+        c = 0
+        for button in self.ADOBE:
+            self.btn = ttk.Button(self.adobe, text=button)
+            self.btn.grid(row=r, column=c, padx=3, pady=3)
+            c += 1
+            if c == 4:
+                r += 1
+                c = 0
+
 
 if __name__ == "__main__":
     app = Window()

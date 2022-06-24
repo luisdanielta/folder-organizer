65826a892468e98d7c15d9ca39f0b8d9c849ff48
luis daniel <torresluisdaniel3@gmail.com>
2022-06-23 23:41:00 -0500

    update

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ from tkinter import Tk
 
 
 class Window(Tk):
-
-    BUTTONS = {"Video": [".mp4", ".avi", ".mov"],
+    BUTTONS = {"Video": [".mp4", ".avi", ".mov", ".MP4", ".AVI", ".MOV"],
                "Audio": [".mp3", ".wav", ".flac", ".ogg", ".aac", ".wma"],
-               "Image": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".psd", ".webp", ".heic", ".heif", ".svg", ".ico", ".PNG", ".JPG", ".JPEG", ".GIF", ".BMP", ".TIFF", ".PSD", ".WEBP", ".HEIC", ".HEIF", ".SVG", ".ICO"],
-               "Document": [".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pdf", ".txt", ".rtf", ".csv", ".html", ".htm", ".xml", ".json", ".yml", ".yaml", ".md", ".markdown"],
-               "Compressed": [".zip", ".rar", ".img", ".dng", ".tar", ".7z"],
-               "Program": [".exe", ".msi", ".app", ".deb", ".apk", ],
-               "Photoshop": [".psd"],
-               "Illustrator": [".ai", ".eps"], }
+               "Image": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".heic", ".heif", ".svg",
+                         ".ico", ".PNG", ".JPG", ".JPEG", ".GIF", ".BMP", ".TIFF", ".WEBP", ".HEIC", ".HEIF",
+                         ".SVG", ".ICO", ".TIFF"],
+               "Document": [".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pdf", ".txt", ".rtf", ".csv", ".html",
+                            ".htm", ".xml", ".json", ".yml", ".yaml", ".md", ".markdown"],
+               "Compressed": [".zip", ".rar", ".img", ".dmg", ".tar", ".7z", ".bin"],
+               "Program": [".exe", ".msi", ".app", ".deb", ".apk", ".jar", ".py", ".bat", ".java"]}
     OS = sys.platform
     PATH: str
+
+    # path protected
+    protected: list = ['C:/', 'C:/Users', '/home', '/Users', '/']
 
     def __init__(self):
         super().__init__()
@@ -34,45 +37,47 @@ class Window(Tk):
         self.exit.grid(row=4, padx=5, pady=5, sticky="NSEW")
 
     def getPath(self):
-        if self.OS == "win32":
-            self.PATH = filedialog.askdirectory(initialdir="C:\\Users")
-        elif self.OS == "linux":
-            self.PATH = filedialog.askdirectory(initialdir="/home")
-        elif self.OS == "darwin":
-            self.PATH = filedialog.askdirectory(initialdir="/Users")
-        else:
-            print("Unsupported OS")
-            self.destroy()
+        username: str
+        typeOS = {'win32': 'C:/Users', 'linux': '/home', 'darwin': '/Users'}
+        error = 'Please select a folder different than the default one.'
+        for element in typeOS:
+            if self.OS == element:
+                self.PATH = filedialog.askdirectory(initialdir=typeOS[element])
+                for i in self.protected:
+                    if i == self.PATH:
+                        messagebox.showerror('Error', error)
+                        return
+                if self.OS == 'win32':
+                    username = self.PATH.split('/')[2]
+                else:
+                    username = self.PATH.split('/')[1]
+                self.protected.append(str(typeOS[element]) + '/' + str(username))
 
-        if self.PATH == 'C:/Users' or self.PATH == '/home' or self.PATH == '/Users':
-            text = "Please select a foldel different than the default one."
-            messagebox.showerror("Error", text)
-            return
+        for i in self.protected:
+            if self.PATH == i:
+                messagebox.showerror('Error', error)
+                return
 
-        # globals() config state of buttons to be enabled
         for button in self.BUTTONS:
             globals()[button].config(state="enabled")
 
-        self.textPath = ttk.Label(self, text=self.PATH)
-        self.textPath.grid(row=1, padx=5, pady=5, sticky="NSEW")
+        message = ttk.Label(self, text=self.PATH)
+        message.grid(row=1, padx=5, pady=5, sticky="NSEW")
 
-        # create folders
-        for button in self.BUTTONS:
-            if button != "All":
-                if not os.path.exists(self.PATH + "\\" + button):
-                    os.makedirs(self.PATH + "\\" + button)
-
-    def organize(self, type):
+    def organize(self, type: str):
         # get files
         files = os.listdir(self.PATH)
         ext = self.BUTTONS[type]
         start = time.time()
 
+        if not os.path.exists(self.PATH + "/" + type):
+            os.makedirs(self.PATH + "/" + type)
+
         for file in files:
             if file.endswith(tuple(ext)):
                 # move file
-                os.rename(self.PATH + "\\" + file, self.PATH +
-                          "\\" + type + "\\" + file)
+                os.rename(self.PATH + "/" + file, self.PATH +
+                          "/" + type + "/" + file)
 
         end = time.time()
         text = "Organization completed in " + str(end - start) + " seconds."
@@ -85,7 +90,7 @@ class Window(Tk):
         c = 0
         for button in self.BUTTONS:
             globals()[button] = ttk.Button(
-                self.frame, text=button, state="disabled", command=lambda button=button: self.organize(button))
+                self.frame, text=button, state="disabled", command=lambda btn=button: self.organize(button))
             globals()[button].grid(row=r, column=c, padx=3, pady=3)
             c += 1
             if c == 4:
